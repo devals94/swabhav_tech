78ddadd5558a0771e11435476a9c4d9d79c0bcbf
deval@SWTL <devals94@gmail.com>
2018-05-07 14:03:20 +0530

    exception-app

--- Core Java/OOP Workspace/exception-app/src/com/techlabs/exception/TestBug_1.java
+++ Core Java/OOP Workspace/exception-app/src/com/techlabs/exception/TestBug_1.java
@@ -2,12 +2,14 @@ package com.techlabs.exception;
 
 public class TestBug_1 {
 	public static void main(String[] args) {
-		try{
-		int a = Integer.parseInt(args[0]);
-		int b = Integer.parseInt(args[1]);
-		int c = a / b;
-		System.out.println(c);
-		}catch(Exception e){
+		try {
+			main(new String[] { "50", "10" }); // calling main from main will
+												// result in StackOverFlowError
+			int a = Integer.parseInt(args[0]);
+			int b = Integer.parseInt(args[1]);
+			int c = a / b;
+			System.out.println(c);
+		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
