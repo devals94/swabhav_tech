4a81dc4b0421d62d16e64440888da5c59a33c0f2
deval@SWTL <devals94@gmail.com>
2018-05-14 14:05:10 +0530

    student-college-app

--- Core Java/OOP Workspace/student-college-app/src/com/techlabs/college/College.java
+++ Core Java/OOP Workspace/student-college-app/src/com/techlabs/college/College.java
@@ -3,7 +3,7 @@ package com.techlabs.college;
 import java.util.ArrayList;
 import java.util.List;
 
-public class College implements EmployeeSalary {
+public class College {
 
 	private List<Student> students = new ArrayList<Student>();
 	private List<Professor> professors = new ArrayList<Professor>();
@@ -23,13 +23,4 @@ public class College implements EmployeeSalary {
 	public List<Professor> displayAllProfessors() {
 		return professors;
 	}
-
-	@Override
-	public List<Professor> calculateSalary(List<Professor> professor) {
-		for (Professor p : professor) {
-			p.setSalary(p.getNoOfHoursWorked() * 200);
-		}
-		return professor;
-	}
-
 }
