947b27a7d92ca2c3e9ebd8fa545ef6e374f901a5
deval@SWTL <devals94@gmail.com>
2018-06-09 17:14:37 +0530

    customized exception

---
+++ Core Java/OOAD Workspace/tic-tac-toe-app-2.0/src/com/game/cell/CellPositionUnavailableException.java
@@ -0,0 +1,10 @@
+package com.game.cell;
+
+@SuppressWarnings("serial")
+public class CellPositionUnavailableException extends Exception {
+
+	public CellPositionUnavailableException(String string) {
+		super(string);
+	}
+
+}
